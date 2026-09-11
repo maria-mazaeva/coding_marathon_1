974603334329761eeeb136b7eb2a87d45a5cfe68
MariaMazaeva <mazaeva.mar@gmail.com>
2026-09-11 22:35:50 +0300

    changed home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,13 @@
 function Home() {
   return (
     <div>
-      <h2>Group 2 ?</h2>
-      <p>Code Marathon 1</p>
+      <h2>Group 2 </h2>
+      <h4>Code Marathon 1</h4>
+      <h5>Done by:</h5>
+        <p>Maria (RecipeManager),</p>
+        <p>Veranika (BookCollectionManager),</p>
+        <p>George (SingupPage),</p>
+        <p>Anastasia (ShoppingCart)</p>
     </div>
   );
 }
